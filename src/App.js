c79fb84d3690743b63f3a7ecbaa700914f9e93ee
20232426 <kaku009@ess.ais.ac.nz>
2024-07-13 08:31:28 +1200

    login.js updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
-import Main from './components/Main';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './Register';
+import Login from './Login';
+import MainForm from './MainForm';
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Main</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </div>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<MainForm />} />
+      </Routes>
     </Router>
   );
 };
