c978cb65636731af4a4f1d7c5e6ba2674f5925f0
20232426 <kaku009@ess.ais.ac.nz>
2024-07-13 06:25:00 +1200

    Register page is created

---
+++ src/components/Main_form.js
@@ -0,0 +1,12 @@
+// assignment1_frontend/src/components/Main.js
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Welcome to the Main Page</h1>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/Register.js
@@ -0,0 +1,39 @@
+// assignment1_frontend/src/components/Register.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/api/register', { username, password })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
