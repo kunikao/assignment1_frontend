e37a4b0ff6cd8e086ca5b2163270714eba4670ac
20232426 <kaku009@ess.ais.ac.nz>
2024-07-13 15:05:27 +1200

    test updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import renderer from 'react-test-renderer'; // Import the renderer
+import App from './App'; // Import the component you want to test
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders correctly', () => {
+  // Render the component
+  const component = renderer.create(<App />);
+
+  // Convert the component to JSON
+  const tree = component.toJSON();
+
+  // Compare the rendered component to a stored snapshot
+  expect(tree).toMatchSnapshot();
 });
