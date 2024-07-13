13898d5390292a77ca04016d2396a54839595860
20232426 <kaku009@ess.ais.ac.nz>
2024-07-14 08:41:37 +1200

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,3 @@
-// Login.js
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -20,13 +19,10 @@ function Login() {
     axios.post(`${BaseUrl}login/`, formData)
       .then((response) => {
         console.log(response.data);
-        // Example: Assuming response.data.token exists for successful login
-        if (response.data.token) {
-          // Handle success (e.g., set user session, store token in localStorage)
-          localStorage.setItem('token', response.data.token); // Example: Storing token in localStorage
-          navigate('/mainform'); // Navigate to mainform after successful login
+        if (response.data.access) {
+          localStorage.setItem('token', response.data.access);
+          navigate('/mainform');
         } else {
-          // Handle error (e.g., show error message)
           alert('Login failed. Invalid credentials.');
         }
       })
@@ -56,9 +52,6 @@ function Login() {
         />
         <button type="submit">Login</button>
       </form>
-      <p>
-        Don't have an account? <Link to="/register">Register here</Link>
-      </p>
     </div>
   );
 }
