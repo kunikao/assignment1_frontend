54571366ba2bd5eb482dca5f2db96fcc2db932da
20232426 <kaku009@ess.ais.ac.nz>
2024-07-31 13:38:44 +1200

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import renderer from 'react-test-renderer'; // Import the renderer
-import App from './App'; // Import the component you want to test
+import renderer from 'react-test-renderer';
+import HelloWorld from "./components/HelloWorld";
+
 
 test('renders correctly', () => {
-  // Render the component
-  const component = renderer.create(<App />);
-
-  // Convert the component to JSON
-  const tree = component.toJSON();
-
-  // Compare the rendered component to a stored snapshot
-  expect(tree).toMatchSnapshot();
+    const component = renderer.create(<HelloWorld />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
